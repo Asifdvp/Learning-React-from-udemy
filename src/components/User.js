030d74fb0d16a0cc0904d3199935713e164208a0
Asif Mustafayev <53805470+Asifdvp@users.noreply.github.com>
2022-06-17 11:17:26 +0400

    this.bind(this,name)

--- src/components/User.js
+++ src/components/User.js
@@ -1,27 +1,45 @@
 import React from "react";
-import PropTypes from 'prop-types'
-export default function User(props){
-  const {name,department,salary} = props;
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+export default class User extends React.Component{
+  state = {
+    isVisible: false
+  }
+handleClick = (num,e) =>{
+console.log(e.target)
+console.log(num)
+}
+  
+render(){
+  const   {name,department,salary} = this.props;
+const {isVisible} = this.state
     return(
-<div className="user">
-<ul>
-<li> Ad:{name} </li>
-<li> department:{department}</li>
-<li> maas:{salary} </li>
+<div className="user col-md-8 mb-4">
+<div className="card">
+    <div className="card-header d-flex justify-content-between">
+        <h4 className="d-inline"   onClick={this.handleClick.bind(this,name)} >{name} </h4>
+        <FontAwesomeIcon icon={faTrash} className="icon" />
 
-    </ul>
+    </div>
 
+    { isVisible ?  <div className="card-body">
+        <p className="card-text">  Department:{department}</p>
+        <p className="card-text"> Salary:{salary}</p>
+         </div> : null}
+   
+</div>
     </div>
     )
     
+}}
+User.defaultProps = {
+    name:"Ad yoxdur",
+    department:" yoxdur",
+    salary:"yoxdur"
 }
 User.propTypes = {
     name:PropTypes.string.isRequired,
     department:PropTypes.string.isRequired,
-    maas:PropTypes.string.isRequired
+    salary:PropTypes.string.isRequired
  }
-User.defaultProps = {
-    name:"Ad yoxdur",
-   department:" yoxdur",
-   salary:"yoxdur",
-}
